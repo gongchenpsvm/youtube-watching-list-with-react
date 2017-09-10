5816850df1b77b90609fc605970e48119ed89e63
gongchen <gongchen@usc.edu>
2017-09-09 17:04:51 -0700

    before sign up Youtube API, create files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from 'react';//React core works with making components
+import ReactDOM from 'react-dom';//React dom shows the components
+//Create new component, which produces some html
+//Take the generated html to page
+//Make and show
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-import App from './components/app';
-import reducers from './reducers';
+//This APP component is a class.
+// ReactDOM.render(App);
+//We need an instance. Add JSX closing tag to make the component instance
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// ReactDOM.render(<App />);
+//But not specified place to put the dom
+//Need a container to place our component instance
+ReactDOM.render(<App />, document.querySelector('.container'));
