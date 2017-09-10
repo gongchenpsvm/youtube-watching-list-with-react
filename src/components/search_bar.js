af5a0200fce43b3dd868390d8afc82306355ce5d
gongchen <gongchen@usc.edu>
2017-09-09 17:50:18 -0700

    functional component to class component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const SearchBar = () => {
-  return <input />;
-};
+/*functional component*/
+// const SearchBar = () => {
+//   return <input />;
+// };
+
+/*class component*/
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
 
 export default SearchBar;
